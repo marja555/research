5ad354d2ffc69b30735dca62eefdb425c5708d12
Bakumenko <tinyy11@gmail.com>
2022-04-05 13:40:46 +0300

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
-// import React from "react";
+import React from "react";
 
-// function Header() {
-  
-// }
-// export default Header;
+function Header() {
+   return (
+      <>
+      <h1 className="header__title">Когнитивные технологии, <span className="header__title-span">машинное обучение</span></h1>
+      <p className="header__subtitle">Системы прикладного искусственного интеллекта</p>
+      <button className="header__button">НАШИ ПРОЕКТЫ</button>
+      </>
+   )
+
+}
+export default Header;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
 function Main(props) {
     return (
       <>
         <nav className="navbar"></nav>
-        <header className="header"></header>
+        <header className="header">
+          <Header/>
+        </header>
         <section className="tech"></section>
         <article className="about-us"></article>
         <section className="lab"></section>
