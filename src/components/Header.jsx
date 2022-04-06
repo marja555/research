803461bff8478810dfda8d3e983b2f540f11553b
BakumenkoVS <tinyyy11@gmail.com>
2022-04-06 18:58:17 +0300

    header version before review

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
 
 function Header() {
    return (
       <>
-      <h1 className="header__title">Когнитивные технологии, <span className="header__title-span">машинное обучение</span></h1>
-      <p className="header__subtitle">Системы прикладного искусственного интеллекта</p>
-      <button className="header__button">НАШИ ПРОЕКТЫ</button>
+         <h1 className="header__title">
+            Когнитивные технологии,{' '}
+            <span className="header__title-span">машинное обучение</span>
+         </h1>
+         <p className="header__subtitle">
+            Системы прикладного искусственного интеллекта
+         </p>
+         <div className="header__button">
+            <a href="#" className="header_link">
+               НАШИ ПРОЕКТЫ
+            </a>
+         </div>
       </>
-   )
-
+   );
 }
 export default Header;
