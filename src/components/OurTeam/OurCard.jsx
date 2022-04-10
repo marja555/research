1b56ff5d338e849c947927b3504ece018950543f
Valery Avdeev <vkavdeev@gmail.com>
2022-04-10 23:30:05 +0300

    feat: imitation of a slider, correcting styles

--- src/components/OurTeam/OurCard.jsx
+++ src/components/OurTeam/OurCard.jsx
@@ -1,8 +1,10 @@
-function OurCard({imageLink, name, position}) {
+function OurCard({imageLink, name, surname, position}) {
 return(
   <div className='our-team-card'>
     <img src={imageLink} className='our-team-card__image' alt={name}/>
     <h3 className='our-team-card__name'>
+      {surname}
+      <br/>
       {name}
     </h3>
     <p className='our-team-card__position'>
--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -6,26 +6,36 @@ import ourTeam2 from '../../images/ourTeam2.jpg';
 function OurTeam() {
   return (
    <>
-    <h2 className='our-team__title'>НАША КОМАНДА</h2>
+    <h2 className='our-team__title'>наша команда</h2>
     <div className='our-team__cards'>
       <OurCard
         imageLink={ourTeam2}
-        name='Бухановский Александр Александр Валерьевич'
+        surname ='Бухановский'
+        name='Александр Александр Валерьевич'
         position='Директор, ДТН'
       />
       <OurCard
         imageLink={ourTeam1}
+        surname
         name='Бобцов Алексей Алексеевич'
         position='Директор, ДТН Директор, ДТН Директор, ДТН Директор, ДТН'
       />
       <OurCard
         imageLink={ourTeam1}
-        name='Бобцов Алексей Алексеевич'
+        surname='Бобцов'
+        name='Алексей Алексеевич'
         position='Директор, ДТН'
       />
       <OurCard
         imageLink={ourTeam1}
-        name='Бобцов Алексей Алексеевич'
+        surname='Бобцов'
+        name='Алексей Алексеевич'
+        position='Директор, ДТН Директор, ДТН Директор, ДТН Директор, ДТН'
+      />
+      <OurCard
+        imageLink={ourTeam1}
+        surname='Бобцов'
+        name='Алексей Алексеевич'
         position='Директор, ДТН Директор, ДТН Директор, ДТН Директор, ДТН'
       />
     </div>
